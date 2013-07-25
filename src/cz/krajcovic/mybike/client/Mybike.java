241c8ce0402a28c0ef701d8c637bb28b847c382e
Krajcovic <dusan.krajcovic@gmail.com>
2013-07-25 13:22:15 +0200

    About page.

--- src/cz/krajcovic/mybike/client/Mybike.java
+++ src/cz/krajcovic/mybike/client/Mybike.java
@@ -1,6 +1,6 @@
-package cz.krajcovic.client;
+package cz.krajcovic.mybike.client;
 
-import cz.krajcovic.shared.FieldVerifier;
+//import cz.krajcovic.mybike.shared.FieldVerifier;
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
@@ -8,6 +8,7 @@ import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.event.dom.client.KeyCodes;
 import com.google.gwt.event.dom.client.KeyUpEvent;
 import com.google.gwt.event.dom.client.KeyUpHandler;
+import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.DialogBox;
@@ -26,6 +27,15 @@ public class Mybike implements EntryPoint {
 	 * This is the entry point method.
 	 */
 	public void onModuleLoad() {
+		Button buttonAbout = new Button("About");
+		buttonAbout.addClickHandler(new ClickHandler() {
+			
+			@Override
+			public void onClick(ClickEvent arg0) {
+				Window.alert("Application MyBike under development");				
+			}
+		});
 		
+		RootPanel.get().add(buttonAbout);
 	}
 }
